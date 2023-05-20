87028a8fd76b211776d2ef609528598c095730f1
HighnessAtharva <HighnessAtharva@gmail.com>
2023-05-20 09:56:41 +0530

    add events

--- data.js
+++ data.js
@@ -8,6 +8,11 @@ const timelineData = {
                     "monthImage": "img/2023-may.jpg",
                     "events": [
                         {
+                            "date": "May 19th",
+                            "description": "Made this project",
+                            "link": "https://highnessatharva.github.io/timeline/"
+                        },
+                        {
                             "date": "May 18th",
                             "description": "Created Elden Ring Character Gallery",
                             "link": "https://highnessatharva.github.io/Elden-Ring-Gallery/"
@@ -64,14 +69,55 @@ const timelineData = {
                     "monthImage": "img/2023-april.jpg",
                     "events": [
                         {
+                            "date": "April 29th",
+                            "description": "Halfway Through HuggingFace Course",
+                            "link": "https://www.youtube.com/playlist?list=PLo2EIpI_JMQvWfQndUesu0nPBAtZ9gP1o"
+                        },
+                        {
+                            "date": "April 25th",
+                            "description": "Django ASETT Challenge for Accuknox - Crypto Portfolio App",
+                            "link": "https://github.com/HighnessAtharva/django-crypto-app"
+                        },
+                        {
+                            "date": "April 20th",
+                            "description": "Tutorial Project - Django Quiz App",
+                            "link": "https://www.youtube.com/playlist?list=PLgjw1dR712jrkldk1GFPQkPsp1RsZ3K1u"
+                        },
+                        {
+                            "date": "April 17th",
+                            "description": "Tutorial Project - Netlix T3 Clone (Next.js)",
+                            "link": "https://github.com/HighnessAtharva/netflix-clone-t3"
+                        },
+                        {
+                            "date": "April 14th",
+                            "description": "Kaggle Certified (Data Visualization)",
+                            "link": "https://www.kaggle.com/learn/certification/highnessatharva/data-visualization"
+                        },
+                        {
+                            "date": "April 12th",
+                            "description": "Kaggle Certified (Data Cleaning)",
+                            "link": "https://www.kaggle.com/learn/certification/highnessatharva/data-cleaning"
+                        },
+                        {
                             "date": "April 9th",
                             "description": "Kaggle Certified (Advanced SQL)",
                             "link": "https://www.kaggle.com/learn/certification/highnessatharva/advanced-sql"
+                        },
+
+                        {
+                            "date": "April 8th",
+                            "description": "FastAPI Challenge for Kimo",
+                            "link": "https://github.com/HighnessAtharva/fastapi-kimo"
                         },
                         {
                             "date": "April 7th",
                             "description": "Kaggle Certified (Intro to SQL)",
                             "link": "https://www.kaggle.com/learn/certification/highnessatharva/intro-to-sql"
+                        },
+                        {
+                            "date": "April 6th",
+                            "description": "Kaggle Certified (Pandas)",
+                            "link": "https://www.kaggle.com/learn/certification/highnessatharva/pandas"
                         },
                         {
                             "date": "April 5th",
@@ -86,10 +132,42 @@ const timelineData = {
                     "monthImage": "img/2023-march.jpg",
                     "events": [
                         {
-                            "date": "DATE",
-                            "description": "EVENT",
+                            "date": "March 30th",
+                            "description": "Took FreeCodeCamp Scrapy Course",
+                            "link": "https://www.youtube.com/watch?v=mBoX_JCKZTE"
+                        },
+                        {
+                            "date": "March 29th",
+                            "description": "Finished Sem VI Exams at University",
                             "link": ""
+                        },
+                        {
+                            "date": "March 20th",
+                            "description": "Revised Basic Python DSA",
+                            "link": "https://github.com/HighnessAtharva/Leetcode_Grind"
+                        },
+                        {
+                            
+                            "date": "March 15th",
+                            "description": "Finished the Django FoodOnline Project",
+                            "link": "https://github.com/HighnessAtharva/FoodOnline"
+                        },
+                        {
+                            "date": "March 10th",
+                            "description": "Added AdSense to Blog, ready to monetize",
+                            "link": ""
+                        },
+                        {
+                            "date": "March 9th",
+                            "description": "Joined TutorialsPoint as a Technical Content Writer",
+                            "link": "https://www.tutorialspoint.com/authors/atharva-shah"
+                        },
+                        {
+                            "date": "March 2nd",
+                            "description": "Started building Media Analysis (Streamlit)",
+                            "link": "https://github.com/HighnessAtharva/Media-Analysis"
                         }
+                            
                     ]
                 },
                 {
@@ -97,10 +175,35 @@ const timelineData = {
                     "monthImage": "img/2023-february.jpg",
                     "events": [
                         {
-                            "date": "DATE",
-                            "description": "EVENT",
+                            "date": "February 26th",
+                            "description": "Finished React Crash Course (FreeCodeCamp)",
+                            "link": "https://www.youtube.com/watch?v=2-crBg6wpp0&"
+                        },
+                        {
+                            "date": "February 19th",
+                            "description": "System Design Topics Revised",
+                            "link": "https://www.youtube.com/playlist?list=PL9nWRykSBSFgkCmkYdEfmXHX71m1UybNL"
+                        },
+                        {
+                            "date": "February 16th",
+                            "description": "JavaScript Nuggets",
+                            "link": "https://www.youtube.com/playlist?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ"
+                        },
+                        {
+                            "date": "February 15th",
+                            "description": "Studied Django Auth and REST APIs",
                             "link": ""
-                        }
+                        },
+                        {
+                            "date": "February 10th",
+                            "description": "Wrapped up development of VocabCLI",
+                            "link": "https://github.com/HighnessAtharva/VocabCLI"
+                        },
+                        {
+                            "date": "February 5th",
+                            "description": "Built my Developer Portfolio site.",
+                            "link": "https://HighnessAtharva.github.io"
+                        },
                     ]
                 },
                 {
@@ -108,10 +211,15 @@ const timelineData = {
                     "monthImage": "img/2023-january.jpg",
                     "events": [
                         {
-                            "date": "DATE",
-                            "description": "EVENT",
-                            "link": ""
-                        }
+                            "date": "January 25th",
+                            "description": "Machine Learning course with Python",
+                            "link": "https://www.youtube.com/playlist?list=PLfFghEzKVmjsNtIRwErklMAN8nJmebB0I"
+                        },
+                        {
+                            "date": "January 13th",
+                            "description": "Won #1 in MindSpark'23 Hackathon",
+                            "link": "https://github.com/HighnessAtharva/CRIF-Hackathon-2023"
+                        },
                     ]
                 }
             ]
